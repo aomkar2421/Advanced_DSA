ec30891bac2934add7cb5096d4ac00e45eaebf44
aomkar2421 <omkarjagtap1651@gmail.com>
2024-01-20 10:24:15 +0530

    20-January

--- src/LLQuestionsPW/DeleteNode.java
+++ src/LLQuestionsPW/DeleteNode.java
@@ -1,6 +1,6 @@
 package LLQuestionsPW;
 
-public class FindNode {
+public class DeleteNode {
 	Node head;
 	static class Node{
 		int val;
@@ -24,39 +24,29 @@ public class FindNode {
 		Node d = new Node(59);
 		Node e = new Node(17);
 		
-		a.next = b; 
+		a.next = b;
 		b.next = c;
 		c.next = d;
 		d.next = e;
+	
+		display(a);
+		deleteNode(b);
+		display(a);
 		
-		Node temp = a;
+	}
+
+	private static void deleteNode(Node temp) {
+		temp.val = temp.next.val;
+		temp.next = temp.next.next;
+	}
+
+	static void display(Node head){
+		Node temp = head;
 		while (temp != null) {
 			System.out.print(temp.val+"->");
 			temp = temp.next;
 		}
-		System.out.println();
-		
-		getNodeFromEnd(a , 3);
-		
-		
+		System.out.println("END\n");
 	}
-
-	private static void getNodeFromEnd(Node head , int index) {
-		int size = 0;
-		Node temp = head;
-		while (temp != null) {
-			temp = temp.next;
-			size++;
-		}
-
-		int m = size-index+1;
-		
-		Node temp1 = head;
-		for (int i = 1; i < m; i++) {
-			temp1 = temp1.next;
-		}
-		System.out.println(temp1.val);
-	}
-
 	
 }
--- src/LLQuestionsPW/FindNode2.java
+++ src/LLQuestionsPW/FindNode2.java
@@ -1,62 +1,67 @@
 package LLQuestionsPW;
 
-public class FindNode {
+import LLQuestionsPW.DeleteNode.Node;
+
+public class FindNode2 {
 	Node head;
 	static class Node{
 		int val;
 		Node next;
-		
+
 		public Node(int val) {
 			this.val = val;
 		}
-		
+
 		public Node(int val, Node next) {
 			this.val = val;
 			this.next = next;
 		}
-		
+
 	}
-	
+
 	public static void main(String[] args) {
 		Node a = new Node(5);
 		Node b = new Node(51);
 		Node c = new Node(8);
 		Node d = new Node(59);
 		Node e = new Node(17);
-		
-		a.next = b; 
+
+		a.next = b;
 		b.next = c;
 		c.next = d;
 		d.next = e;
+
+		display(a);
+		getNodeFromEnd(a , 3);
+		display(a);
+
+	}
+
+	private static void getNodeFromEnd(Node head,int number) {
+		Node fast = head;
+		Node slow = head;
+
+		for (int i = 0; i < number; i++) {
+			fast = fast.next;
+		}
 		
-		Node temp = a;
+		
+		while (fast != null) {
+			slow = slow.next;
+			fast = fast.next;
+		}
+		
+		System.out.println(slow.val);
+		
+	}
+
+	static void display(Node head){
+		Node temp = head;
 		while (temp != null) {
 			System.out.print(temp.val+"->");
 			temp = temp.next;
 		}
-		System.out.println();
-		
-		getNodeFromEnd(a , 3);
-		
-		
+		System.out.println("END\n");
 	}
 
-	private static void getNodeFromEnd(Node head , int index) {
-		int size = 0;
-		Node temp = head;
-		while (temp != null) {
-			temp = temp.next;
-			size++;
-		}
-
-		int m = size-index+1;
-		
-		Node temp1 = head;
-		for (int i = 1; i < m; i++) {
-			temp1 = temp1.next;
-		}
-		System.out.println(temp1.val);
-	}
-
-	
 }
