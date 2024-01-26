3389a1dc1445d439f26caca0f197498023baa3d5
aomkar2421 <omkarjagtap1651@gmail.com>
2024-01-26 13:11:21 +0530

    26-January

---
+++ src/LLQuestionsPW/DeleteMiddle.java
@@ -0,0 +1,58 @@
+package LLQuestionsPW;
+
+public class DeleteMiddle {
+	Node head;
+	static class Node{
+		int val;
+		Node next;
+
+		public Node(int val) {
+			this.val = val;
+		}
+
+		public Node(int val, Node next) {
+			this.val = val;
+			this.next = next;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		Node a = new Node(5);
+		Node b = new Node(51);
+		Node c = new Node(8);
+		Node d = new Node(59);
+		Node e = new Node(17);
+		Node f = new Node(7);
+
+		a.next = b;
+		b.next = c;
+		c.next = d;
+		d.next = e;
+		e.next = f;
+
+		display(a);
+		deleteMiddleNode(a);
+
+	}
+	
+	static void deleteMiddleNode(Node head) {
+		Node slow = head;
+		Node fast = head;
+		
+		while ( fast.next !=null && fast!=null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		System.out.println(slow.val);
+	}
+
+	static void display(Node head){
+		Node temp = head;
+		while (temp != null) {
+			System.out.print(temp.val+"->");
+			temp = temp.next;
+		}
+		System.out.println("END\n");
+	}
+}
---
+++ src/ankita/Template.java
@@ -0,0 +1,46 @@
+package ankita;
+
+
+public class Template {
+	Node head;
+	static class Node{
+		int val;
+		Node next;
+
+		public Node(int val) {
+			this.val = val;
+		}
+
+		public Node(int val, Node next) {
+			this.val = val;
+			this.next = next;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		Node a = new Node(5);
+		Node b = new Node(51);
+		Node c = new Node(8);
+		Node d = new Node(59);
+		Node e = new Node(17);
+
+		a.next = b;
+		b.next = c;
+		c.next = d;
+		d.next = e;
+
+		display(a);
+
+	}
+
+	static void display(Node head){
+		Node temp = head;
+		while (temp != null) {
+			System.out.print(temp.val+"->");
+			temp = temp.next;
+		}
+		System.out.println("END\n");
+	}
+
+}
