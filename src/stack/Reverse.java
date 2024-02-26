f7712d29268acf3204d0fe5e6b50819ea4a92f69
aomkar2421 <omkarjagtap1651@gmail.com>
2024-02-26 21:18:05 +0530

    26 February

--- src/stack/Reverse.java
+++ src/stack/Reverse.java
@@ -6,17 +6,21 @@ public class Reverse {
 	public static void main(String[] args) {
 		
 		Stack<Integer> st = new Stack<Integer>();
+		
 		st.push(5);
 		st.push(9);
 		st.push(18);
 		st.push(51);
+		
 		System.out.println(st);
+		
 		
 		Stack<Integer> dt = new Stack<Integer>();
 		while (st.size()>=1) {
 			dt.push(st.pop());
 		}
 		System.out.println(dt);
+		
 		
 		Stack<Integer> rt = new Stack<Integer>();
 		while (dt.size()>=1) {
